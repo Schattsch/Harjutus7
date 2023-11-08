e36b0a0bcf256b676ffc20f6e7b7475deafa0ff1
John Doe <johndoe@email.com>
2023-11-08 09:17:55 +0200

    Exercise 7.1: Class Dictionary

---
+++ src/com/karel/program/Dictionary.java
@@ -0,0 +1,19 @@
+package com.karel.program;
+
+import java.util.HashMap;
+
+public class Dictionary {
+    private HashMap<String, String> translations;
+
+    public Dictionary() {
+        this.translations = new HashMap<>();
+    }
+
+    public void add(String word, String translation) {
+        translations.put(word, translation);
+    }
+
+    public String translate(String word) {
+        return translations.get(word);
+    }
+}
