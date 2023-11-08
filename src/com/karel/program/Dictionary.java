5cf40df83d3de43cfdc5b0d891e9e00120cf5daf
John Doe <johndoe@email.com>
2023-11-08 09:36:56 +0200

    Exercise 7.3: Listing All Words

--- src/com/karel/program/Dictionary.java
+++ src/com/karel/program/Dictionary.java
@@ -1,5 +1,6 @@
 package com.karel.program;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Dictionary {
@@ -19,5 +20,13 @@ public class Dictionary {
 
     public int amountOfWords() {
         return translations.size();
+    }
+
+    public ArrayList<String> translationList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (String key : translations.keySet()) {
+            list.add(key + " = " + translations.get(key));
+        }
+        return list;
     }
 }
