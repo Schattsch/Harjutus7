118edd3aeb3ac3b89b824b2437df4fec3c924877
John Doe <johndoe@email.com>
2023-11-08 10:34:30 +0200

    Exercise 7.5: Adding and Translating Words

--- src/com/karel/program/TextUserInterface.java
+++ src/com/karel/program/TextUserInterface.java
@@ -4,21 +4,39 @@ import java.util.Scanner;
 
 public class TextUserInterface {
     private Scanner reader;
+    private Dictionary dictionary;
+
     public TextUserInterface(Scanner reader, Dictionary dictionary) {
         this.reader = reader;
+        this.dictionary = dictionary;
     }
 
     public void start() {
-        System.out.println("Statement:");
-        System.out.println("  quit - quit the text user interface");
+        System.out.println("Statements:");
+        System.out.println("  add - adds a word pair to the dictionary");
+        System.out.println("  translate - asks a word and prints its translation");
+        System.out.println("  quit - quits the text user interface");
 
         while (true) {
             System.out.print("Statement: ");
-            String input = reader.nextLine();
+            String statement = reader.nextLine();
 
-            if (input.equals("quit")) {
+            if (statement.equals("quit")) {
                 System.out.println("Cheers!");
                 break;
+            } else if (statement.equals("add")) {
+                System.out.print("In Finnish: ");
+                String word = reader.nextLine();
+                System.out.print("Translation: ");
+                String translation = reader.nextLine();
+
+                dictionary.add(word, translation);
+            } else if (statement.equals("translate")) {
+                System.out.print("Give a word: ");
+                String wordToTranslate = reader.nextLine();
+                String translation = dictionary.translate(wordToTranslate);
+
+                System.out.println("Translation: " + translation);
             } else {
                 System.out.println("Unknown statement");
             }
